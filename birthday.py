2ea4adbf1882acd557af4b8a5cf05d237c1c9fc0
Ash <guptashark@gmail.com>
2015-05-01 14:21:36 -0400

    Temporarily fix error display for invalid date.

--- birthday.py
+++ birthday.py
@@ -14,6 +14,11 @@ months = [
 	'November',
 	'December']
 
+# The bad_date thing is a hacky error message solution. 
+bad_date = """
+<div style="color: red">That is not a valid date.</div>
+"""
+
 def valid_month(month):
 	if (month):
 		myMonth = month.capitalize()
@@ -38,30 +43,11 @@ def valid_year(year):
 
 
 class Birthday(Handler):
+#	def write_form(error=""):
+#	self.render("birthday.html", 
+
 	def get(self):
 		self.render("birthday.html")
-
-#	def valid_month(month):
-#		if (month):
-#			myMonth = month.capitalize()
-#			for i in months:
-#				if (myMonth == i):
-#					return myMonth
-#		return None
-#
-#	def valid_day(day):
-#		if (day and day.isdigit()):
-#			myDay = int(day)
-#			if(myDay >= 1 and myDay <= 31):
-#				return myDay
-#		return None
-	
-#	def valid_year(year):
-#		if (year and year.isdigit()):
-#			mYear = int(year)
-#			if(mYear >= 1900 and mYear <= 2020):
-#				return mYear
-#		return None
 
 	def post(self):
 		month = valid_month(self.request.get("month"))
@@ -71,3 +57,4 @@ class Birthday(Handler):
 			self.write("ALL FINE")
 		else:
 			self.render("birthday.html")
+			self.write(bad_date)  # Hacky solution to get error message. 
