023992757220f6cb81e14cd18bf7c030350acb33
Ash <guptashark@gmail.com>
2015-05-01 13:40:01 -0400

    make the birthday valid input checking work.

--- birthday.py
+++ birthday.py
@@ -14,6 +14,29 @@ months = [
 	'November',
 	'December']
 
+def valid_month(month):
+	if (month):
+		myMonth = month.capitalize()
+		for i in months:
+			if (myMonth == i):
+				return myMonth
+	return None
+
+def valid_day(day):
+	if (day and day.isdigit()):
+		myDay = int(day)
+		if(myDay >= 1 and myDay <= 31):
+			return myDay
+	return None
+
+def valid_year(year):
+	if (year and year.isdigit()):
+		mYear = int(year)
+		if(mYear >= 1900 and mYear <= 2020):
+			return mYear
+	return None
+
+
 class Birthday(Handler):
 	def get(self):
 		self.render("birthday.html")
@@ -41,10 +64,10 @@ class Birthday(Handler):
 #		return None
 
 	def post(self):
-		month = self.request.get("month")
-		day = self.request.get("day")
-		year = self.request.get("year")
-#		if(valid_month(month) and valid_day(day) and valid_year(year)):
-		self.write("ALL FINE, SRSLY, pls believe")
-#		else:
-#			self.render("birthday.html")
+		month = valid_month(self.request.get("month"))
+		day = valid_day(self.request.get("day"))
+		year = valid_year(self.request.get("year"))
+		if(month and day and year):
+			self.write("ALL FINE")
+		else:
+			self.render("birthday.html")
