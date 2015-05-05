84d9299601aaaa4d9f5a28c811463dd4757ffbb4
Ash <guptashark@gmail.com>
2015-05-05 00:57:27 -0400

    Make rot13 work by using builtin string.encode('rot13')

--- rot13.py
+++ rot13.py
@@ -1,31 +1,23 @@
 import webapp2
 import string
 
-lowercase = "abcdefghijklmnopqrstuvwxyz"
-uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 rot13_form = """
 <form method="post">
 	<h1>Enter some text to ROT13:</h1>
-	<textarea name="text" cols="50" rows="10">
-	</textarea>
+	<textarea name="text" cols="50" rows="10">%(plain)s</textarea>
 	<br>
 	<input type="submit">
 </form>
 """
 
-def encode(plainText):
-	rot13 = string.maketrans(
-			"ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-			"NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-	return string.translate(plainText, rot13)
-
-
 class Rot13(webapp2.RequestHandler):
+	def write_form(self, plain=""):
+		self.response.write(rot13_form % {"plain" : plain})
+
 	def get(self):
-		self.response.write(rot13_form)
+		self.write_form();
 	
 	def post(self):
 		plainText = self.request.get("text")
-		cipherText = encode(plainText)
-		self.response.write(ciperText)
+		plain = plainText.encode('rot13')
+		self.write_form(plain)
