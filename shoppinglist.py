2abf1ada901fdb2a80a3d0f2ca75f7aaaab25d9e
guptashark <guptashark@gmail.com>
2015-05-09 08:33:06 -0400

    Create basic shopping list functionality

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,13 +1,42 @@
 from handler import Handler
 
-html_form = """
+form_html = """
 <form>
 	<h2>Add a Food</h2>
 	<input type="text" name="food">
+	%s
 	<button>Add</button>
 </form>
 """
 
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
+"""
+
 class ShoppingList(Handler):
 	def get(self):
-		self.write(html_form)
+		output = form_html
+		output_hidden = ""
+
+		items = self.request.get_all("food")
+		if items:
+			output_items = ""
+			for item in items:
+				output_hidden += hidden_html % item
+				output_items += item_html %item
+			output_shopping = shopping_list_html % output_items
+			output += output_shopping
+
+		output = output % output_hidden
+			
+		self.write(output)
