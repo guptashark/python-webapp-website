8178e339108ce70f421fb336d77e56867480172b
guptashark <guptashark@gmail.com>
2015-05-09 10:14:12 -0400

    Small example of if statements in html jinja2 templating

--- shoppinglist.py
+++ shoppinglist.py
@@ -25,18 +25,22 @@ shopping_list_html = """
 
 class ShoppingList(Handler):
 	def get(self):
-		output = form_html
-		output_hidden = ""
+		n = self.request.get("n")
+		if n:
+			n = int(n)
+		self.render("shoppinglist.html", n=n)
 
-		items = self.request.get_all("food")
-		if items:
-			output_items = ""
-			for item in items:
-				output_hidden += hidden_html % item
-				output_items += item_html %item
-			output_shopping = shopping_list_html % output_items
-			output += output_shopping
-
-		output = output % output_hidden
-			
-		self.write(output)
+#		output = form_html
+#		output_hidden = ""
+#
+#		items = self.request.get_all("food")
+#		if items:
+#			output_items = ""
+#			for item in items:
+#				output_hidden += hidden_html % item
+#				output_items += item_html %item
+#			output_shopping = shopping_list_html % output_items
+#			output += output_shopping
+#		output = output % output_hidden
+#			
+#		self.write(output)
